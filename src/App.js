a847ebaa8e7aff63d7424ac7e313b4ea360098c7
Sergey Kravchenko <s.kravchenko@outlook.com>
2020-03-31 17:14:07 +0300

    webpack: basic src

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello ReactJS</h1>
+    </div>
+  );
+}
+
+export default App;
